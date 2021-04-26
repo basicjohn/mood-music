77aa5f829452f73b8449cb630cb5f9ad113bf2c2
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-26 16:29:25 -0700

    add embed function to spotify embed logic

---
+++ src/spotify-embed.js
@@ -0,0 +1,8 @@
+export default class Embed {
+  embed(uri) {
+    let uri = undefined;
+    return embedScript = `<iframe src="https://open.spotify.com/embed/${uri}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
+  }
+}
+
+// template embed script <iframe src="https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
