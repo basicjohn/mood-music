51c8e86046b3ebe048f5fb7ea69fa3a2e262c635
basicjohn <edmondsonj@gmail.com>
2021-04-27 14:34:22 -0700

    update main

--- src/main.js
+++ src/main.js
@@ -3,23 +3,25 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 // import SpotifyService from "./spotify-service.js";
+//let spotifyKeywords = ["devastated", "coping", "heartache", "dark", "boost", "alone", "spooky", "crazy", "stress", "sad", "tender", "commute", "cleaning", "rainy day", "focus", "bed", "beats", "morning", "wake up", "work from home", "coffeehouse", "chill", "porch", "feels", "good vibes", "smile", "ballads", "caviar", "warm", "park", "feel-good", "date night", "dinner", "happy", "greatest", "perfect", "wild", "bad bitch", "cardio", "workout", "gameday", "tailgate", "champs", "beautiful", "love", "confidence", "party", "songs in the shower"];
+let inputKeywords = ["devastated", "mournful", "heartbroken", "dark", "beleaguered", "alone", "scared", "crazy", "stressed", "sad", "tender", "irritated", "fastidious", "bored", "preoccupied", "lazy", "studious", "sleepy", "eager", "busy", "undercaffeinated", "relaxed", "curious", "groovy", "dreamy", "playful", "emotional", "defiant", "cute", "cheerful", "upbeat", "sexy", "nostalgiac", "happy", "timeless", "perfect", "adventurous", "sassy", "driven", "hardcore", "intense", "territorial", "triumphant", "beautiful", "amorous", "confident", "outgoing", "euphoric"];
 
-function storeKeywords() {
-  let spotifyKeywords = ["devastated", "coping", "heartache", "dark", "boost", "alone", "spooky", "crazy", "stress", "sad", "tender", "commute", "cleaning", "rainy day", "focus", "bed", "beats", "morning", "wake up", "work from home", "coffeehouse", "chill", "porch", "feels", "good vibes", "smile", "ballads", "caviar", "warm", "park", "feel-good", "date night", "dinner", "happy", "greatest", "perfect", "wild", "bad bitch", "cardio", "workout", "gameday", "tailgate", "champs", "beautiful", "love", "confidence", "party", "songs in the shower"];
-  sessionStorage.setItem("spotifyKeywords", spotifyKeywords);
-  let inputKeywords = ["devastated", "mournful", "heartbroken", "dark", "beleaguered", "alone", "scared", "crazy", "stressed", "sad", "tender", "irritated", "fastidious", "bored", "preoccupied", "lazy", "studious", "sleepy", "eager", "busy", "undercaffeinated", "relaxed", "curious", "groovy", "dreamy", "playful", "emotional", "defiant", "cute", "cheerful", "upbeat", "sexy", "nostalgiac", "happy", "timeless", "perfect", "adventurous", "sassy", "driven", "hardcore", "intense", "territorial", "triumphant", "beautiful", "amorous", "confident", "outgoing", "euphoric"];
-  sessionStorage.setItem("inputKeywords", inputKeywords);
-}
+// function storeKeywords() {
+//   let spotifyKeywords = ["devastated", "coping", "heartache", "dark", "boost", "alone", "spooky", "crazy", "stress", "sad", "tender", "commute", "cleaning", "rainy day", "focus", "bed", "beats", "morning", "wake up", "work from home", "coffeehouse", "chill", "porch", "feels", "good vibes", "smile", "ballads", "caviar", "warm", "park", "feel-good", "date night", "dinner", "happy", "greatest", "perfect", "wild", "bad bitch", "cardio", "workout", "gameday", "tailgate", "champs", "beautiful", "love", "confidence", "party", "songs in the shower"];
+//   sessionStorage.setItem("spotifyKeywords", spotifyKeywords);
+//   let inputKeywords = ["devastated", "mournful", "heartbroken", "dark", "beleaguered", "alone", "scared", "crazy", "stressed", "sad", "tender", "irritated", "fastidious", "bored", "preoccupied", "lazy", "studious", "sleepy", "eager", "busy", "undercaffeinated", "relaxed", "curious", "groovy", "dreamy", "playful", "emotional", "defiant", "cute", "cheerful", "upbeat", "sexy", "nostalgiac", "happy", "timeless", "perfect", "adventurous", "sassy", "driven", "hardcore", "intense", "territorial", "triumphant", "beautiful", "amorous", "confident", "outgoing", "euphoric"];
+//   sessionStorage.setItem("inputKeywords", inputKeywords);
+// }
 
-function getKeyword(inputKeyword) {
-  let spotifyKeywords = sessionStorage.getItem(spotifyKeywords);
-  let inputKeywords = sessionStorage.getItem(inputKeywords);
-  for (let i = 0; i < spotifyKeywords.length; i++) {
-    if (inputKeywords[i].includes(inputKeyword)) {
-      return spotifyKeywords[i];
-    }
-  }
-}
+// function getKeyword(inputKeyword) {
+//   let spotifyKeywords = sessionStorage.getItem(spotifyKeywords);
+//   let inputKeywords = sessionStorage.getItem(inputKeywords);
+//   for (let i = 0; i < spotifyKeywords.length; i++) {
+//     if (inputKeywords[i].includes(inputKeyword)) {
+//       return spotifyKeywords[i];
+//     }
+//   }
+// }
 
 // function runSpotify() {
 //   SpotifyService.getAuthToken().then(function (token) {
@@ -41,13 +43,13 @@ function getKeyword(inputKeyword) {
 // UI Logic
 $(document).ready(function () {
   let rangeInput = $("#valenceRange").val();
-  console.log(rangeInput);
+  console.log(inputKeywords.length);
   $("#valenceOutput").text(rangeInput);
 
 
   $(document).on('input', '#valenceRange', function() {
     
-    $('#valenceOutput').html( $(this).val() );
+    $('#valenceOutput').html( inputKeywords[$(this).val()] );
 
   });
 });
