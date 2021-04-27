5f14899a1ca612139e9b073aed9155bfbe67793b
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-27 09:03:31 -0700

    remove variable declaration from embed()

--- src/spotify-embed.js
+++ src/spotify-embed.js
@@ -1,6 +1,5 @@
 export default class Embed {
   embed(uri) {
-    let uri = undefined;
     return embedScript = `<iframe src="https://open.spotify.com/embed/${uri}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
   }
 }
