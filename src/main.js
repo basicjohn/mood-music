61b8378860fd37dead536da76db9de7d42e7ee29
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-27 13:58:24 -0700

    store input and Spotify keywords in sessionStorage

--- src/main.js
+++ src/main.js
@@ -4,6 +4,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import SpotifyService from "./spotify-service.js";
 
-SpotifyService.getAuthToken().then(function (response) {
-  console.log(response);
-});
+function storeKeywords() {
+  let spotifyKeywords = ["devastated", "coping", "heartache", "dark", "boost", "alone", "spooky", "crazy", "stress", "sad", "tender", "commute", "cleaning", "rainy day", "focus", "bed", "beats", "morning", "wake up", "work from home", "coffeehouse", "chill", "porch", "feels", "good vibes", "smile", "ballads", "caviar", "warm", "park", "feel-good", "date night", "dinner", "happy", "greatest", "perfect", "wild", "bad bitch", "cardio", "workout", "gameday", "tailgate", "champs", "beautiful", "love", "confidence", "party", "songs in the shower"];
+  sessionStorage.setItem("spotifyKeywords", spotifyKeywords);
+  let inputKeywords = ["devastated", "mournful", "heartbroken", "dark", "beleaguered", "alone", "scared", "crazy", "stressed", "sad", "tender", "irritated", "fastidious", "bored", "preoccupied", "lazy", "studious", "sleepy", "eager", "busy", "undercaffeinated", "relaxed", "curious", "groovy", "dreamy", "playful", "emotional", "defiant", "cute", "cheerful", "upbeat", "sexy", "nostalgiac", "happy", "timeless", "perfect", "adventurous", "sassy", "driven", "hardcore", "intense", "territorial", "triumphant", "beautiful", "amorous", "confident", "outgoing", "euphoric"];
+  sessionStorage.setItem("inputKeywords", inputKeywords);
+}
+
+function runSpotify() {
+  SpotifyService.getAuthToken().then(function (token) {
+    SpotifyService.getPlaylistWithTokenAndKeyword(token, "chill").then(
+      function (data) {
+        if (data) {
+          console.log(data.playlists.items);
+        } else {
+          console.log("Still...WTF!?");
+        }
+      }
+    );
+  });
+}
+
+runSpotify();
