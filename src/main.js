3b1b5b613619c6f6e0bfaff6b59bfb5064cf44ce
Nicholas Reeder <nickreeder32@gmail.com>
2021-04-26 17:02:44 -0700

    get recommendations for keyword using authtoken

--- src/main.js
+++ src/main.js
@@ -4,6 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import SpotifyService from "./spotify-service.js";
 
-SpotifyService.getAuthToken().then(function (response) {
-  console.log(response);
-});
+function runSpotify() {
+  SpotifyService.getAuthToken().then(function (token) {
+    SpotifyService.getPlaylistWithTokenAndKeyword(token, "chill").then(
+      function (data) {
+        if (data) {
+          console.log(data.playlists.items);
+        } else {
+          console.log("Still...WTF!?");
+        }
+      }
+    );
+  });
+}
+
+runSpotify();
--- src/spotify-service.js
+++ src/spotify-service.js
@@ -1,18 +1,39 @@
 var ClientOAuth2 = require("client-oauth2");
 
 export default class SpotifyService {
-  constructor() {
-    this.getAuthToken();
-  }
-  static async getAuthToken() {
+  static getAuthToken() {
     var githubAuth = new ClientOAuth2({
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       grantType: "client_credentials",
       accessTokenUri: "https://accounts.spotify.com/api/token",
     });
-    githubAuth.credentials.getToken().then(function (user) {
-      return user.accessToken;
-    });
+    return githubAuth.credentials
+      .getToken()
+      .then(function (user) {
+        if (!user) {
+          throw new Error(user.error);
+        }
+        return user.data.access_token;
+      })
+      .catch((err) => console.log(err));
+  }
+  static getPlaylistWithTokenAndKeyword(authToken, keyword) {
+    return fetch(
+      `https://api.spotify.com/v1/browse/categories/${keyword}/playlists?country=US&limit=5`,
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      }
+    )
+      .then(function (response) {
+        if (!response) {
+          throw new Error(response.status);
+        }
+        //console.log(`response: ${response.body}`);
+        return response.json();
+      })
+      .catch((err) => console.log(`err = ${err}`));
   }
 }
