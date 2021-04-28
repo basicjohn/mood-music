234ac3183197f4e4bd9811b3b95f5b4f6b49200a
Nicholas Reeder <nickreeder32@gmail.com>
2021-04-28 16:49:22 -0700

    functionining w/out ahmed api and new styling

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import SpotifyService from "./spotify-service";
 import Playlists from "./playlists.js";
 import Playlist from "./playlist.js";
 import Constants from "./constants.js";
-import TasteDiveService from "./taste-dive-service";
+//import TasteDiveService from "./taste-dive-service";
 // Helper logic
 const createPlaylists = (list) => {
   let playLists = new Playlists();
@@ -61,7 +61,7 @@ $(document).ready(function () {
   let constants = new Constants();
   let sliderIndex = 0;
   let rangeInput = $("#valenceRange").val();
-  $("#valenceOutput").text(rangeInput);
+  $("#valenceOutput").html(constants.inputKeywords[rangeInput]);
   getAuthToken();
 
   $(document).on("input", "#valenceRange", function () {
@@ -83,68 +83,17 @@ $(document).ready(function () {
   });
 });
 
-function getElements(response) {
-  if (response.main) {
-    console.log(response);
-  } else {
-    console.log("Error");
-  }
-}
+// function getElements(response) {
+//   if (response.main) {
+//     console.log(response);
+//   } else {
+//     console.log("Error");
+//   }
+// }
 
-async function makeApiCall(movieTitle) {
-  const response = await TasteDiveService.getSimilarMovies(movieTitle);
-  getElements(response);
-}
+// async function makeApiCall(movieTitle) {
+//   const response = await TasteDiveService.getSimilarMovies(movieTitle);
+//   getElements(response);
+// }
 
-makeApiCall(bookTitle);
-
-const movieMoodTitles = [
-  "The Sky Is Pink",
-  "Bringing Up Baby",
-  "Like crazy",
-  "The Mist",
-  "Pitch Perfect",
-  "Lost In Translation",
-  "Ginger Snaps",
-  "Fight Club",
-  "Buried",
-  "My Life",
-  "Illegal Tender",
-  "The Commuter",
-  "Sunshine Cleaning",
-  "A Rainy Day In New York",
-  "Fifty Shades Of Grey",
-  "Her Side Of The Bed",
-  "Moonlight",
-  "The Hangover",
-  "Nobody",
-  "Paid In Full",
-  "Coffee Shop",
-  "Drinking Buddies",
-  "Baby Blues",
-  "The Feels",
-  "Mamma Mia!",
-  "A Smile Like Yours",
-  "Black Rain",
-  "The Diary Of A Teenage Girl",
-  "Amélie",
-  "Jurassic Park",
-  "Booksmart",
-  "Hitch",
-  "The Dinner",
-  "Begin Again",
-  "The Proposal",
-  "Paddington",
-  "Wild",
-  "Won't You Be My Neighbor?",
-  "Fast And Furious",
-  "Rocky",
-  "The Other Guys",
-  "Samaria",
-  "The Champ",
-  "Children Of Heaven",
-  "The Photograph",
-  "Sex And The City",
-  "Sisters",
-  "The Tallest Man On Earth",
-];
+// makeApiCall(bookTitle);
